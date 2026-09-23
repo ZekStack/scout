ff940ab265bfd76e5091daca6e25054f1a90ba26
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-09-23 15:02:24 +0200

    Use explicit typed device resets

--- src/Scout.cpp
+++ src/Scout.cpp
@@ -382,8 +382,8 @@ struct ScoutImpl {
 		if (index != lastIndex) {
 			devices[index] = devices[lastIndex];
 		}
-		devices[lastIndex].info = {};
-		devices[lastIndex].details = {};
+		devices[lastIndex].info = ScoutDeviceInfo{};
+		devices[lastIndex].details = ScoutDeviceDetails{};
 		deviceCount--;
 		diag.deviceCount = deviceCount;
 		identityDirty = true;
@@ -524,8 +524,8 @@ struct ScoutImpl {
 					discovered = true;
 					auto &info = devices[index].info;
 					auto &details = devices[index].details;
-					info = {};
-					details = {};
+					info = ScoutDeviceInfo{};
+					details = ScoutDeviceDetails{};
 					info.key.kind = ScoutIdentityKind::Mac;
 					info.key.mac = scout_internal::macFromBytes(mac);
 					info.mac = info.key.mac;
