ecc35e17adee392466b52314c78f9c70fc031ae4
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-09-25 08:49:28 +0200

    Expose provider resolver and identity diagnostics

--- src/Scout.h
+++ src/Scout.h
@@ -531,6 +531,8 @@ struct ScoutProviderDiagnostics {
 	uint64_t malformedResponses = 0;
 	uint64_t serverErrors = 0;
 	uint64_t droppedObservations = 0;
+	uint64_t resolverUnavailable = 0;
+	uint64_t identityConflicts = 0;
 	uint64_t budgetYields = 0;
 	uint64_t cancellations = 0;
 };
